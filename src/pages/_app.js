1dfa7eee934f4e6c01586114c63739d8fd96eeab
Nejer <Nejer6@gmail.com>
2023-08-26 00:39:16 +0300

    delete: insecure-requests

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,5 @@
 import '@/styles/globals.css'
-import {Head} from "next/document";
 
 export default function App({ Component, pageProps }) {
-  return <>
-    <Head>
-      <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
-    </Head>
-    <Component {...pageProps} />
-  </>
-
-
+  return <Component {...pageProps} />
 }

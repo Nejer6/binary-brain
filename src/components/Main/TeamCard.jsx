25118559f77dc9a4e39bbaef113f8fbf536fd7aa
Nejer <Nejer6@gmail.com>
2023-08-25 23:54:57 +0300

    refactor: adaptive for our services and team

--- src/components/Main/TeamCard.jsx
+++ src/components/Main/TeamCard.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 const TeamCard = ({name, description, src}) => {
     return (
         <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
-
+            <div className="w-full px-10">
                 <img
                     className="w-full object-cover"
                     src={src}
                     alt=""
                 />
+            </div>
+
 
 
             <div className="text-center text-sky-400 text-4xl uppercase my-5 font-tektur">{name}</div>

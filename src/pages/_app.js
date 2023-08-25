754d0d0128140be7dae29b47324d290a33a73bd9
Nejer <Nejer6@gmail.com>
2023-08-26 00:31:40 +0300

    add: insecure-requests

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import {Head} from "next/document";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+    </Head>
+    <Component {...pageProps} />
+  </>
+
+
 }

7abf1ebbef4e7a4a0f411b7edf4e1270a5a7122b
Nejer <Nejer6@gmail.com>
2023-08-26 15:37:24 +0300

    add: accept

--- src/components/Main/ModalWindow.jsx
+++ src/components/Main/ModalWindow.jsx
@@ -36,6 +36,7 @@ const ModalWindow = ({onClick, successHandler}) => {
         const requestOptions = {
             method: 'POST',
             headers: {
+                'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data),

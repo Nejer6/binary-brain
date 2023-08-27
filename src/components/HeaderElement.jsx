637e7d8b9ac0e37bd5e449b23e82bda926e23ce5
Nejer <Nejer6@gmail.com>
2023-08-27 15:35:07 +0300

    add: animation for header elements

--- src/components/HeaderElement.jsx
+++ src/components/HeaderElement.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import Link from "next/link";
 
 const HeaderElement = ({href, text, isSelected, onClick, className}) => {
+    const styles = `h-full w-full duration-300 transition-all hover:pb-4 hover:pt-2 flex items-end pb-2 pt-4 justify-center text-center cursor-pointer hover:underline px-2 ${isSelected ?
+        "bg-sky-400 text-white" :
+        "text-sky-400 bg-white"}`
+
     return (
         <div className={className}>
             {href && <Link
                 href={href}
-                className={`h-full w-full flex items-end justify-center text-center cursor-pointer hover:underline px-2 ${isSelected ?
-                    "bg-sky-400 text-white" :
-                    "text-sky-400 bg-white"}`}
+                className={styles}
                 onClick={onClick}
             >{text}</Link>}
 
             {!href && <a
                 href={href}
-                className={`h-full w-full flex items-end justify-center text-center cursor-pointer hover:underline px-2 ${isSelected ?
-                    "bg-sky-400 text-white" :
-                    "text-sky-400 bg-white"}`}
+                className={styles}
                 onClick={onClick}
             >{text}</a>}
         </div>

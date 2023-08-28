b8ed69db43cd335106020fe4d3718d4ac1bf4e5b
Nejer <Nejer6@gmail.com>
2023-08-28 17:00:28 +0300

    fix: order header

--- src/components/Order/HeaderOrder.js
+++ src/components/Order/HeaderOrder.js
@@ -24,7 +24,7 @@ const HeaderMain = () => {
     return (
         <header className={`flex flex-wrap justify-center sm:justify-between lg:px-16 fixed z-20 w-full bg-white top-0`}>
             <div className={`flex flex-col items-center mx-3 lg:mx-0 my-3`}>
-                <object type="image/svg+xml" data="/logo.svg" className={`transition-all duration-300 ${scrollingDown ? 'max-h-0' : 'max-h-60'} `}/>
+                <object type="image/svg+xml" data="/logo.svg" className={`h-[38px] md:h-[64px]  transition-all duration-300 ${scrollingDown ? 'max-h-0' : 'max-h-60'} `}/>
                 <div
                     className="cursor-pointer text-center text-sky-400 text-sm leading-snug md:text-2xl md:leading-9 font-tektur"
                     onClick={() => {
